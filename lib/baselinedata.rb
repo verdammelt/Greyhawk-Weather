13747454cf530c33584065ff42737abd3ed64882
Mark Simpson <verdammelt@gmail.com>
2010-10-14 08:46:59 -0400

    Moved baseline data into yaml data file - maybe in the future it might be a real db...

--- lib/baselinedata.rb
+++ lib/baselinedata.rb
@@ -1,21 +1,11 @@
+
 class BaselineData
   def initialize
-    @base_temps = [32, 34, 42, 52, 63, 71, 77, 75, 68, 57, 46, 33]
-    @base_temp_ranges = [{ :high => [10, 0], :low => [20, 0]}, 
-                         { :high => [6, 4], :low => [10, 4]},
-                         { :high => [8, 4], :low => [10, 4]},
-                         { :high => [10, 6], :low => [8, 4]},
-                         { :high => [10, 6], :low => [10, 6]},
-                         { :high => [8, 8], :low => [6, 6]},
-                         { :high => [6, 4], :low => [6, 6]},
-                         { :high => [4, 6], :low => [6, 6]},
-                         { :high => [8, 6], :low => [8, 6]},
-                         { :high => [10, 5], :low => [10, 5]},
-                         { :high => [10, 6], :low => [10, 4]},
-                         { :high => [8, 5], :low => [20, 0]}]
+    @all_data = YAML.load_file("./data/baselinedata.yml")
   end
 
   def month(monthnum)
-    Month.new @base_temps[monthnum - 1], @base_temp_ranges[monthnum -1]
+    month_data = @all_data[monthnum -1]
+    Month.new month_data[:base], month_data[:range]
   end
 end
