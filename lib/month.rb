bbd3a481468e728da09d5038ca649c2643bfb003
Mark Simpson <verdammelt@gmail.com>
2010-11-02 21:19:22 -0400

    initial implementation of sky conditions.  Not entirely happy - don't want to add all the baseline data until i'm sure this is how i want to do it.

--- lib/month.rb
+++ lib/month.rb
@@ -1,10 +1,16 @@
 class Month
   attr_reader :temperature
   attr_reader :range_deltas
+  attr_reader :sky_conditions_ranges
 
-  def initialize(temperature, range_deltas = { :high => [0, 0], :low => [0, 0] })
+  def initialize(temperature, range_deltas = { :high => [0, 0], :low => [0, 0] }, sky_condition_ranges = { :clear => (0..99)})
     @temperature = temperature
     @range_deltas = range_deltas
+    @sky_condition_ranges = sky_condition_ranges
+  end
+  
+  def conditions (dieroll)
+    @sky_condition_ranges.find{ |kv| kv[1].include?(dieroll)}[0]
   end
 end
 
--- lib/weather.rb
+++ lib/weather.rb
@@ -3,6 +3,7 @@ require 'month'
 class Weather
   attr_reader :temperature_range
   attr_reader :base_temperature
+  attr_reader :sky_conditions
   
   def initialize (month, dieroller)
     @dieroller = dieroller
@@ -14,11 +15,12 @@ class Weather
     @temperature_range = 
       Range.new(month.temperature - dieroller.roll(low_range[0], low_range[1]),
                 month.temperature + dieroller.roll(high_range[0], high_range[1]))
+
+    @sky_conditions = month.conditions(dieroller.roll(100, 0))
   end
-                
   
   def dump
-    p [@base_temperature, @temperature_range]
+    p [@base_temperature, @temperature_range, @sky_conditions]
   end
 end
 
