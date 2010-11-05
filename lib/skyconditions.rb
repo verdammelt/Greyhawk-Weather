798cb63b593ee52fa6c5412d8e1c83109217233a
Mark Simpson <verdammelt@gmail.com>
2010-11-05 19:16:35 -0400

    Removing a debug print statement i forgot to last time.

--- lib/skyconditions.rb
+++ lib/skyconditions.rb
@@ -6,15 +6,15 @@ class SkyConditions
   end
   
   def condition(roll)
-    
-    p @clear_range.class
-
     roll = 100 if roll == 0
     case roll
     when @clear_range then :clear
     when @partly_range then :partly_cloudy
     when @cloudy_range then :cloudy
-    else "ERROR: SkyCondition.condition " + roll.to_s + " doesn't match ranges " + @clear_range.to_s + " or " + @partly_range.to_s + " or " + @cloudy_range.to_s
+    else "ERROR: SkyCondition.condition " + roll.to_s \
+      + " doesn't match ranges " + @clear_range.to_s \
+      + " or " + @partly_range.to_s \
+      + " or " + @cloudy_range.to_s
     end
   end
 end
