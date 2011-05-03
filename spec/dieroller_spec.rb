776fded5fb315db32a52805139f8754088016b6b
Mark Simpson <verdammelt@gmail.com>
2011-05-02 20:45:19 -0400

    changing from test/unit to rspec for DieRoller

--- spec/dieroller_spec.rb
+++ spec/dieroller_spec.rb
@@ -1,11 +1,21 @@
-require 'spec_helper'
-
 require 'util/dieroller'
 
 describe DieRoller do
   it "should use Kernel::rand" do
     subject.stub(:rand).and_return 0
-    subject.should_receive(:rand)
+    subject.should_receive(:rand).with(6)
     subject.roll(6)
+  end
+
+  it "should return random number +1" do
+    subject.stub(:rand).and_return 0
+    subject.should_receive (:rand)
+    subject.roll(6).should == 1
+  end
+
+  it "should apply modifier if given" do
+    subject.stub(:rand).and_return 1
+    subject.roll(6, 2).should == 4
+    subject.roll(6, -3).should == -1
   end
 end
