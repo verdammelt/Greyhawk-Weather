6d220a1e533706f3c15c8abcb8a918798201ea36
Mark Simpson <verdammelt@gmail.com>
2011-05-04 22:26:47 -0400

    switching from test/unit to rspec for month

---
+++ spec/month_spec.rb
@@ -0,0 +1,33 @@
+require 'month'
+
+describe Month do
+  it "uses sky condition data to determine current conditions" do
+    month = Month.new(nil, SkyConditions.new((0..34), (35..65), (66..100)))
+    month.sky_conditions(make_roller(25)).should == :clear
+    month.sky_conditions(make_roller(50)).should == :partly_cloudy
+    month.sky_conditions(make_roller(75)).should == :cloudy
+  end
+
+  it "uses temp range data to determine current conditions" do
+    month = Month.new(TemperatureRange.new(10, [8, 2], [20, 5]), nil)
+    month.temp_range(make_roller(5)).should == (0..17)
+  end
+
+  it "handles record high" do
+    month = Month.new(TemperatureRange.new(10, [8, 2], [20, 5]), nil)
+    month.temp_range(make_roller(1), :high).should == (14..23)
+  end
+
+  it "determines precipitation" do
+    month = Month.new(nil, nil, 50)
+    month.has_precipitation(make_roller(10)).should be_true
+    month.has_precipitation(make_roller(70)).should be_false
+  end
+
+  private
+  def make_roller(roll)
+    roller = mock(:DieRoller)
+    roller.stub(:roll) { |n, m = 0| roll + m}
+    roller
+  end
+end 
